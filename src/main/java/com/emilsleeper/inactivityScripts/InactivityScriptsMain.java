01d52dab284950b8cfc2c9654a5275289d4d4cec
Emilthesleeper <emilwege@icloud.com>
2025-12-17 17:49:58 +0100

    Initial commit

---
+++ src/main/java/com/emilsleeper/inactivityScripts/InactivityScriptsMain.java
@@ -0,0 +1,17 @@
+package com.emilsleeper.inactivityScripts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InactivityScriptsMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
